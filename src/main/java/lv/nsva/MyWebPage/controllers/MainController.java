21c8e7a3f764d1d78543597ff062e4937bbde648
Vadims Gostevs <vadims.gostevs@gmail.com>
2023-06-12 12:05:28 +0300

    Step_3: Added thymeleaf fragments (blocks): header, footer. Some changes in style.

--- src/main/java/lv/nsva/MyWebPage/controllers/MainController.java
+++ src/main/java/lv/nsva/MyWebPage/controllers/MainController.java
@@ -15,8 +15,8 @@ public class MainController {
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "Main page");
-        return "home";
+        model.addAttribute("title", "About page");
+        return "about";
     }
 
 }
